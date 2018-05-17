53733b8074ec0d1b25588296cc57b455ccce7e05
binyam <bsh385@gmail.com>
2018-05-17 05:40:06 +0300

    Some bug fixes

--- src/main/java/edu/mum/asd/libraryframework/dao/IDAOFactory.java
+++ src/main/java/edu/mum/asd/libraryframework/dao/IDAOFactory.java
@@ -3,4 +3,6 @@ package edu.mum.asd.libraryframework.dao;
 public interface IDAOFactory {
 public IItemDAO getItemDAO(String ItemType);
 public AuthorDao getAuthoDAO();
+public ReservationDao getReservationDAO();
+public UserDao getUserDAO();
 }
